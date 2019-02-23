a498ecd7848fbfb67300057bf9b9184c721548ad
Marek Sontag <mareksontag@gmail.com>
2019-02-23 23:10:13 +0100

    Initial commit

---
+++ src/main/java/pl/sda/spring/mvc0/Mvc0Application.java
@@ -0,0 +1,13 @@
+package pl.sda.spring.mvc0;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mvc0Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mvc0Application.class, args);
+	}
+
+}
