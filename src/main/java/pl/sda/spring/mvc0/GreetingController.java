6aefd35b06faf2e88346690cf36c2d6b78f058f0
Marek Sontag <mareksontag@gmail.com>
2019-02-23 23:12:15 +0100

    Zad 5.3

---
+++ src/main/java/pl/sda/spring/mvc0/GreetingController.java
@@ -0,0 +1,15 @@
+package pl.sda.spring.mvc0;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class GreetingController {
+
+    @GetMapping("/greeting")
+    public String greeting() {
+        return "greeting";
+    }
+}
